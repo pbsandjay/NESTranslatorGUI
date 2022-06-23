64cc6ae877865e9fa2b9712abdd3ac979388c415
pbsandjay <48532071+pbsandjay@users.noreply.github.com>
2022-06-23 16:30:52 -0400

    Finished...

---
+++ Documentation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NES_Decom_GUI
+{
+    public partial class Documentation : Form
+    {
+        public Documentation()
+        {
+            InitializeComponent();
+        }
+
+        private void Documentation_Load(object sender, EventArgs e)
+        {
+            
+            webBrowser2.Navigate(new Uri(@"https://pbsandjay.github.io/"));
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+    }
+}
